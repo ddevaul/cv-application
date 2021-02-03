3700961bce3eafcd88f20cbf643186ed1ae6cebe
Desi <desi.devaul@gmail.com>
2021-02-03 01:10:00 -0500

    Made it possible to add or delete college minors.

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import uniqid from "uniqid";
 
 export default class Education extends React.Component {
   constructor(props){
@@ -7,20 +8,46 @@ export default class Education extends React.Component {
       editing: true,
       college: "Princeton",
       gradYear: "2024",
-      studying: "Classics", // could make this an array of objects or something like that 
+      major: "Classics",
+      minors: [], 
       gpa: "4.20",
     }
   }
+
   handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    const name = e.target.name;
+    if (name.includes('minors')) {
+      const tempArray = this.state.minors;
+      // get the final character, which is the index
+      tempArray[name[name.length - 1]].minorName = e.target.value;
+      this.setState({
+        minors: tempArray
+      });
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      })
+    }
   }
+
   toggleEditing = () => {
     this.setState({editing: !this.state.editing});
   }
+
+  addMinor = () => {
+    this.setState({minors: [...this.state.minors, {minorName: "New Minor", id: uniqid()}]});
+  }
+
+  deleteMinor = (e) => {
+    this.setState({minors: this.state.minors.filter((minor) => minor.id === e.target.id ? false : true)});
+  }
+  
   render(){
-    const {editing, college, gradYear, studying, gpa} = this.state;
+    const {editing, college, gradYear, major, minors, gpa} = this.state;
+    let minorsLabel = null;
+    if(minors.length > 0){
+      minorsLabel = <label>Minors: </label>
+    }
     if (editing) {
       return (
         <div>
@@ -33,9 +60,17 @@ export default class Education extends React.Component {
             <input name="gradYear"  onChange={this.handleChange} value={gradYear}></input>
           </label>
           <label>
-            Studied: 
-           <input name="studying"  onChange={this.handleChange} value={studying}></input>
+            Major: 
+           <input name="major"  onChange={this.handleChange} value={major}></input>
           </label>
+          {minorsLabel}
+            {minors.map((minor, index) => 
+              {return <div key={minor.id}>
+                <input name={`minors${index}`} value={minor.minorName} onChange={this.handleChange}></input>
+                <button id={minor.id} onClick={this.deleteMinor}>delete</button>
+              </div>}
+                )}
+          <button onClick={this.addMinor}>Add a Minor</button>
           <label>GPA:
            <input name="gpa"  onChange={this.handleChange} value={gpa}></input>
           </label>
@@ -47,7 +82,9 @@ export default class Education extends React.Component {
       <div>
        <div>{college}</div>
        <div>{gradYear}</div>
-       <div>{studying}</div>
+       <div>{major}</div>
+      {minors.map((minor) => <div key={minor.id}>{minor.minorName}</div>
+          )}
        <div>{gpa}</div>
        <button type="button" onClick={this.toggleEditing}>Edit</button>
       </div>
